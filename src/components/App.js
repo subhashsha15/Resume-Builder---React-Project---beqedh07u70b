af7022e4383ee8e802a0f62f314bf5f1a4814265
Subhash Kumar <subhashsha15@gmail.com>
2023-06-27 13:42:41 +0000

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,7 @@
-import React from 'react'
-import '../styles/App.css';
+import React from "react";
+import "../styles/App.css";
 const App = () => {
-
-
-  return (
-    <div id="main">
-    </div>
-  )
-}
-
+  return <div id="main">fdsgusdbugsdb</div>;
+};
 
 export default App;
